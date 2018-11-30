e3f52063e2819999f2931252ef8bd479eeafe6e9
Christoph Rönsch <christoph.roensch@arcor.de>
2018-11-30 01:13:08 +0100

    php cli/fpm like official
     - exts installed more safely
     - libsodium added by default
     - fpm configuration improved

--- build-one.sh
+++ build-one.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-mkdir -p tmp
+mkdir -p tmp/$1
 
-cat $1/minor.sh | docker run -i --entrypoint="/bin/sh" $1:latest > tmp/minor-latest.txt
-cat $1/patch.sh | docker run -i --entrypoint="/bin/sh" $1:latest > tmp/patch-latest.txt
+cat $1/minor.sh | docker run -i --entrypoint="/bin/sh" $1:latest > tmp/$1/minor-latest.txt
+cat $1/patch.sh | docker run -i --entrypoint="/bin/sh" $1:latest > tmp/$1/patch-latest.txt
 
-DOCKER_IMAGE_ID=$(docker build -q $1)
+export DOCKER_IMAGE_ID=$(docker build -q $1)
 
-cat $1/minor.sh | docker run -i --entrypoint="/bin/sh" $DOCKER_IMAGE_ID > tmp/minor-$DOCKER_IMAGE_ID.txt
-cat $1/patch.sh | docker run -i --entrypoint="/bin/sh" $DOCKER_IMAGE_ID > tmp/patch-$DOCKER_IMAGE_ID.txt
+cat $1/minor.sh | docker run -i --entrypoint="/bin/sh" $DOCKER_IMAGE_ID > tmp/$1/minor-$DOCKER_IMAGE_ID.txt
+cat $1/patch.sh | docker run -i --entrypoint="/bin/sh" $DOCKER_IMAGE_ID > tmp/$1/patch-$DOCKER_IMAGE_ID.txt
 
-diff -b --unchanged-line-format= --old-line-format="- %L" --new-line-format="+ %L" tmp/minor-latest.txt tmp/minor-$DOCKER_IMAGE_ID.txt > tmp/minor.txt
-diff -b --unchanged-line-format= --old-line-format="- %L" --new-line-format="+ %L" tmp/patch-latest.txt tmp/patch-$DOCKER_IMAGE_ID.txt > tmp/patch.txt
+diff -b --unchanged-line-format= --old-line-format="- %L" --new-line-format="+ %L" tmp/$1/minor-latest.txt tmp/$1/minor-$DOCKER_IMAGE_ID.txt > tmp/$1/minor.txt
+diff -b --unchanged-line-format= --old-line-format="- %L" --new-line-format="+ %L" tmp/$1/patch-latest.txt tmp/$1/patch-$DOCKER_IMAGE_ID.txt > tmp/$1/patch.txt
