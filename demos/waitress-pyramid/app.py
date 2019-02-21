9ca05b8084141d2aeacd712f56727f04daee8a31
Christoph Rönsch <christoph.roensch@arcor.de>
2019-02-22 00:59:11 +0100

    python: just fixing up the demo

--- demos/waitress-pyramid/app.py
+++ demos/waitress-pyramid/app.py
@@ -10,5 +10,4 @@ if __name__ == '__main__':
     config.add_route('hello', '/')
     config.add_view(hello_world, route_name='hello')
     app = config.make_wsgi_app()
-    # listen *:8080
-    serve(app)
+    serve(app, listen='*:8080')
